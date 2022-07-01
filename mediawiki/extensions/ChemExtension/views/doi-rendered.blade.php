6937ec3a8145ef7810dccedc0d82f49358a14b39
Kai Kühn <kuehn@diqa-pm.com>
2022-07-01 11:34:23 +0200

    changed rendering of literature references

--- mediawiki/extensions/ChemExtension/views/doi-rendered.blade.php
+++ mediawiki/extensions/ChemExtension/views/doi-rendered.blade.php
@@ -1,16 +1,24 @@
-<a href="https://dx.doi.org/{{$doi}}" target="_blank"><div id="literature_{{$index}}" class="chem_ext_literature"><span>[{{$index}}] </span><span class="chem-extension-literature-title">{{$title}}</span>
-@foreach($authors as $author)
-        <span class="chem-extension-literature-small">{{$author}}</span>,&nbsp;
-@endforeach
-@if ($journal != '')
-<span class="chem-extension-literature-small">{{$journal}},</span>
-@endif
-@if ($volume != '')
-<span class="chem-extension-literature-small">Vol. {{$volume}},</span>
-@endif
-@if ($pages != '')
-<span class="chem-extension-literature-small">Pages {{$pages}},</span>
-@endif
-<span class="chem-extension-literature-small">{{$year}}</span>
-</div>
+<a href="https://dx.doi.org/{{$doi}}" target="_blank">
+    <div id="literature_{{$index}}" class="chem_ext_literature"><span>[{{$index}}] </span><span
+                class="chem-extension-literature-title">{!! $title !!}.</span>
+        @foreach($authors as $author)
+            <span class="chem-extension-literature-small">{{$author}}, </span>
+        @endforeach
+        @if ($journal != '')
+            @if($volume == '' && $pages == '')
+                <span class="chem-extension-literature-small">{{$journal}} {{$year}}.</span>
+            @else
+                <span class="chem-extension-literature-small">{{$journal}} {{$year}}, </span>
+            @endif
+        @endif
+        @if ($volume != '' && $pages != '')
+            <span class="chem-extension-literature-small">Vol. {{$volume}}, Pages {{$pages}}.</span>
+        @endif
+        @if ($volume != '' && $pages == '')
+            <span class="chem-extension-literature-small">Vol. {{$volume}}.</span>
+        @endif
+        @if ($volume == '' && $pages != '')
+            <span class="chem-extension-literature-small">Pages {{$pages}}.</span>
+        @endif
+    </div>
 </a>
