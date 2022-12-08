75d5e052ccd1a99a3962acf55209a79cc101202b
Kai Kühn <kuehn@diqa-pm.com>
2022-12-08 16:55:14 +0100

    runSetupForAllWikis

---
+++ bin/runSetupForWiki.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+if [ ! -e $MEDIAWIKI/maintenance/update.php ]; then
+   if [ ! -e ../mediawiki/maintenance/update.php ]; then
+       echo Error: update.php script not found
+       exit 1
+   else
+    export RUNJOBSCRIPT=../mediawiki/maintenance/update.php
+   fi
+else
+export RUNJOBSCRIPT=$MEDIAWIKI/maintenance/update.php
+fi
+
+
+
+if [ -z "$1" ]
+  then
+    echo "No wikiId supplied"
+    exit
+fi
+
+export REQUEST_URI=/$1/mediawiki
+echo "Running setup for wiki $1"
+php $RUNJOBSCRIPT --quick
