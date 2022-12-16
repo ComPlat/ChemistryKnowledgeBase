a327f64a12ea1351cba6adef077559ffc5c1e465
Kai Kühn <kuehn@diqa-pm.com>
2022-12-16 09:14:19 +0100

    displayed molecules

--- mediawiki/extensions/ChemExtension/src/Utils/WikiTools.php
+++ mediawiki/extensions/ChemExtension/src/Utils/WikiTools.php
@@ -18,6 +18,7 @@ class WikiTools {
     public static function sanitizeHTML($html) {
         if (self::isInVisualEditor()) {
             $html = str_replace(array("<tbody>", "</tbody>"), "", $html);
+            $html = strip_tags($html, '<table><tr><th><td>');
         }
         return str_replace("\n", "", $html);
     }
