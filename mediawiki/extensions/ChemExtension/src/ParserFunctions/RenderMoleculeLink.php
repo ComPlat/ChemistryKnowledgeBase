da7ab1c2af699c2406c96da3bb7f13458b7eefb7
Kai Kühn <kuehn@diqa-pm.com>
2023-03-02 12:36:32 +0100

    parameter for rendering image in molecule link

--- mediawiki/extensions/ChemExtension/src/ParserFunctions/RenderMoleculeLink.php
+++ mediawiki/extensions/ChemExtension/src/ParserFunctions/RenderMoleculeLink.php
@@ -2,7 +2,6 @@
 
 namespace DIQA\ChemExtension\ParserFunctions;
 
-use DIQA\ChemExtension\Literature\DOITools;
 use DIQA\ChemExtension\Pages\ChemFormRepository;
 use DIQA\ChemExtension\Utils\WikiTools;
 use MediaWiki\MediaWikiServices;
@@ -19,25 +18,32 @@ class RenderMoleculeLink
         array_shift($parametersAsStringArray); // get rid of Parser
         $parameters = ParserFunctionParser::parseArguments($parametersAsStringArray);
 
+        $dbr = MediaWikiServices::getInstance()->getDBLoadBalancer()->getConnection(DB_REPLICA);
+        $chemFormRepo = new ChemFormRepository($dbr);
         if (isset($parameters['link']) && $parameters['link'] != '') {
-            $dbr = MediaWikiServices::getInstance()->getDBLoadBalancer()->getConnection(DB_REPLICA);
-            $chemFormRepo = new ChemFormRepository($dbr);
             $chemformId = $chemFormRepo->getChemFormId($parameters['link']);
+            if (is_null($chemformId)) {
+                return self::returnAsHTML("Molecule with key {$parameters['link']} does not exist.");
+            }
         } else if (isset($parameters['chemformid']) && $parameters['chemformid'] != '') {
             $chemformId = $parameters['chemformid'];
+            if (is_null($chemformId)) {
+                return self::returnAsHTML("Molecule with chemformId {$parameters['chemformid']} does not exist.");
+            }
         } else {
-            return ["-missing link parameter-", 'noparse' => true, 'isHTML' => true];
+            return self::returnAsHTML("-missing link parameter-");
         }
 
-        if (is_null($chemformId)) {
-            return ["Molecule with key {$parameters['link']} does not exist.", 'noparse' => true, 'isHTML' => true];
+        $moleculeKey = $chemFormRepo->getMoleculeKey($chemformId);
+        if (is_null($moleculeKey)) {
+            return self::returnAsHTML("Molecule with chemformId {$chemformId} does not exist.");
         }
 
         $page = Title::newFromText("$chemformId", NS_MOLECULE);
         if (!$page->exists()) {
             $page = Title::newFromText("$chemformId", NS_REACTION);
             if (!$page->exists()) {
-                return ["Molecule with ID $chemformId does not exist.", 'noparse' => true, 'isHTML' => true];
+                return self::returnAsHTML("Molecule with ID $chemformId does not exist.");
             }
         }
 
@@ -51,19 +57,26 @@ class RenderMoleculeLink
             $html = $page->getText();
 
         } else {
-
+            global $wgScriptPath;
             $html = $blade->view()->make("molecule-link",
                 [
                     'url' => $page->getFullURL(),
                     'label' => $page->getText(),
-                    'fullPageTitle' => $page->getPrefixedText()
+                    'fullPageTitle' => $page->getPrefixedText(),
+                    'imageURL' => $wgScriptPath . "/rest.php/ChemExtension/v1/chemform?moleculeKey=" . urlencode($moleculeKey),
+                    'image' => ($parameters['image'] ?? false) === "true",
+                    'width' => $parameters['width'] ?? 300,
+                    'height' => $parameters['height'] ?? 200,
                 ]
             )->render();
 
         }
 
         $html = str_replace("\n", "", $html);
-        return [$html, 'noparse' => true, 'isHTML' => true];
+        return self::returnAsHTML($html);
     }
 
+    private static function returnAsHTML($text) {
+        return [$text, 'noparse' => true, 'isHTML' => true];
+    }
 }
--- mediawiki/extensions/ChemExtension/views/molecule-link.blade.php
+++ mediawiki/extensions/ChemExtension/views/molecule-link.blade.php
@@ -1,1 +1,5 @@
+@if($image)
+<a title="{{$fullPageTitle}}" target="_blank" href="{{$url}}"><img src="{{$imageURL}}" width="{{$width}}" height="{{$height}}"/></a>
+@else
 <a target="_blank" href="{{$url}}" title="{{$fullPageTitle}}">{{$label}}</a>
+@endif
