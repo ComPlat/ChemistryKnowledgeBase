97dc0e6b908c0559efe97335b8906850b2684879
Kai Kühn <kuehn@diqa-pm.com>
2023-06-30 15:57:52 +0200

    changed order of button/description of experimentlink

--- mediawiki/extensions/ChemExtension/views/experiment-link-table.blade.php
+++ mediawiki/extensions/ChemExtension/views/experiment-link-table.blade.php
@@ -2,7 +2,7 @@
     <div>No experiment found</div>
 @else
 <div class="experiment-link-border">
-    <div class="experiment-link-control-bar"><div>{{$description}}</div> <div>{!! $button !!}</div></div>
+    <div class="experiment-link-control-bar"><div>{!! $button !!}</div><div>{{$description}}</div></div>
     <div class="experiment-link-container" id="ce-show-investigation-{{$buttonCounter}}-table">
       {!! $htmlTableEditor->toHtml() !!}
     </div>
