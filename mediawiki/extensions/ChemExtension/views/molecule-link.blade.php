429c2dada42c994949a03cfd7656b17bd9fdfc96
Kai Kühn <kuehn@diqa-pm.com>
2023-07-06 15:58:43 +0200

    added molecule-ID to image

--- mediawiki/extensions/ChemExtension/views/molecule-link.blade.php
+++ mediawiki/extensions/ChemExtension/views/molecule-link.blade.php
@@ -1,5 +1,7 @@
-@if($image)
+@if($image)<div>
 <a title="{{$fullPageTitle}}" target="_blank" href="{{$url}}"><img src="{{$imageURL}}" width="{{$width}}" height="{{$height}}"/></a>
+<div><a style="font-weight: bold" target="_blank" href="{{$url}}" title="{{$fullPageTitle}}">{{$label}}</a></div>
+</div>
 @else
 <a target="_blank" href="{{$url}}" title="{{$fullPageTitle}}">{{$label}}</a>
 @endif
