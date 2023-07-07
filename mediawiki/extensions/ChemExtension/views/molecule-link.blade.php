e8ef8981161e1b6b096a036bf944d9d286cf6ad3
Kai Kühn <kuehn@diqa-pm.com>
2023-07-07 16:27:19 +0200

    molecule links no line break

--- mediawiki/extensions/ChemExtension/views/molecule-link.blade.php
+++ mediawiki/extensions/ChemExtension/views/molecule-link.blade.php
@@ -1,7 +1,7 @@
-@if($image)<div>
+@if($image)<span style="display: inline-grid;">
 <a title="{{$fullPageTitle}}" target="_blank" href="{{$url}}"><img src="{{$imageURL}}" width="{{$width}}" height="{{$height}}"/></a>
-<div><a style="font-weight: bold" target="_blank" href="{{$url}}" title="{{$fullPageTitle}}">{{$label}}</a></div>
-</div>
+<a style="font-weight: bold" target="_blank" href="{{$url}}" title="{{$fullPageTitle}}">{{$label}}</a>
+</span>
 @else
 <a target="_blank" href="{{$url}}" title="{{$fullPageTitle}}">{{$label}}</a>
 @endif
