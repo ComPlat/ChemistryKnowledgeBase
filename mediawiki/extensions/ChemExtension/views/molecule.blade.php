aba9f9b2df24f72d1d5b1549c120631c32591bbf
Kai Kühn <kuehn@diqa-pm.com>
2023-08-04 10:48:35 +0200

    fix: linefeed in molecule template (3)

--- mediawiki/extensions/ChemExtension/views/molecule.blade.php
+++ mediawiki/extensions/ChemExtension/views/molecule.blade.php
@@ -3,5 +3,5 @@
 </span>@else<span style="display: inline-grid;">
 <a title="{{$fullPageTitle}}" target="_blank" href="{{$url}}"><img src="{{$imageURL}}" width="{{$width}}" height="{{$height}}"/></a>
 <a style="font-weight: bold; text-decoration: underline" target="_blank" href="{{$url}}" title="{{$fullPageTitle}}">{{$label}}</a>
-@if($showrgroups)<a class="rgroups-button" style="font-weight: bold; text-decoration: underline" title="{{$fullPageTitle}}" moleculekey="{{$moleculeKey}}>
+@if($showrgroups)<a class="rgroups-button" style="font-weight: bold; text-decoration: underline" title="{{$fullPageTitle}}" moleculekey="{{$moleculeKey}}" pageid="{{$pageId}}">[Show R-Groups]</a>@endif
 </span>@endif
