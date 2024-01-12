a29204525752ff5bd199bfd18d67cb318e6e98fb
Kai Kühn <kuehn@diqa-pm.com>
2024-01-12 11:36:34 +0100

    bugfix

--- mediawiki/extensions/ChemExtension/views/molecule-collection-table.blade.php
+++ mediawiki/extensions/ChemExtension/views/molecule-collection-table.blade.php
@@ -15,7 +15,7 @@
                 <td><a target="_blank" href="{{$row['molecule']->getFullURL()}}">{{$row['molecule']->getText()}}</a></td>
                 <td>
                     @if(count($row['publications']) === 1)
-                        <a target="_blank" href="{{$row['publications'][0]->getFullURL()}}">{{$row['publications'][0]->getText()}}</a>
+                        <a target="_blank" href="{{reset($row['publications'])->getFullURL()}}">{{reset($row['publications'])->getText()}}</a>
                     @else
                         <ul>
                         @foreach($row['publications'] as $p)
