3054112a8fa31a90d3688604e67aeaaafff2910f
Kai Kühn <kuehn@diqa-pm.com>
2024-08-23 17:33:56 +0200

    Electrolyte and Internal ref compound to molecules

--- mediawiki/LocalSettings.php
+++ mediawiki/LocalSettings.php
@@ -390,6 +390,8 @@ $wgPageFormsAutocompletionURLs['molecule_lookup_ed'] = $wgServer . $wgScriptPath
 $wgPageFormsAutocompletionURLs['molecule_lookup_hd'] = $wgServer . $wgScriptPath . '/rest.php/ChemExtension/v1/search-molecule?searchText=<substr>&priorityProperties[]=Hydrogen_donor';
 $wgPageFormsAutocompletionURLs['molecule_lookup_solvent'] = $wgServer . $wgScriptPath . '/rest.php/ChemExtension/v1/search-molecule?searchText=<substr>&priorityProperties[]=Solvent';
 $wgPageFormsAutocompletionURLs['molecule_lookup_analyte'] = $wgServer . $wgScriptPath . '/rest.php/ChemExtension/v1/search-molecule?searchText=<substr>&priorityProperties[]=Analyte';
+$wgPageFormsAutocompletionURLs['molecule_lookup_electrolyte'] = $wgServer . $wgScriptPath . '/rest.php/ChemExtension/v1/search-molecule?searchText=<substr>&priorityProperties[]=Electrolyte';
+$wgPageFormsAutocompletionURLs['molecule_lookup_irc'] = $wgServer . $wgScriptPath . '/rest.php/ChemExtension/v1/search-molecule?searchText=<substr>&priorityProperties[]=IRC';
 
 #################################################################
 # Chemistry
