2e12eb583723c722a00754ba42b99ce0795a6632
Kai Kühn <kuehn@diqa-pm.com>
2026-08-28 16:58:16 +0200

    improve search filters

--- mediawiki/extensions/ChemExtension/src/Endpoints/GetPublications.php
+++ mediawiki/extensions/ChemExtension/src/Endpoints/GetPublications.php
@@ -38,19 +38,24 @@ class GetPublications extends SimpleHandler
             }
         }
 
-        $searchtext = strtolower($params['searchTerm']);
-        $searchtext = $dbr->addQuotes("%$searchtext%");
+        $searchText = strtolower($params['searchTerm']);
+        $parts = explode(' ', $searchText);
+        $parts = array_filter($parts, fn($e) => trim($e) !== '');
 
-        $conds = [
+        $conditions = [
             'page.page_id = page_props.pp_page',
-            "LOWER(CONVERT(pp_value USING latin1)) LIKE $searchtext"
         ];
+        foreach ($parts as $part) {
+            $encoded = $dbr->addQuotes("%$part%");
+            $conditions[] =   "LOWER(CONVERT(pp_value USING latin1)) LIKE $encoded";
+
+        }
         $tables = ['page_props', 'page'];
         if (isset($category_id)) {
             $tables[] = 'category_index';
-            $conds[] = "page.page_id = category_index.page_id AND category_index.category_id = $category_id";
+            $conditions[] = "page.page_id = category_index.page_id AND category_index.category_id = $category_id";
         }
-        $res = $dbr->select($tables, ['DISTINCT page_title', 'page_namespace'], $conds);
+        $res = $dbr->select($tables, ['DISTINCT page_title', 'page_namespace'], $conditions);
         $results = [];
         foreach ($res as $row) {
             $title = Title::newFromText($row->page_title, $row->page_namespace);
--- mediawiki/extensions/ChemExtension/src/NavigationBar/InvestigationFinder.php
+++ mediawiki/extensions/ChemExtension/src/NavigationBar/InvestigationFinder.php
@@ -7,6 +7,7 @@ use MediaWiki\MediaWikiServices;
 use eftec\bladeone\BladeOne;
 use Title;
 use OutputPage;
+use Wikimedia\Rdbms\IDatabase;
 
 class InvestigationFinder
 {
@@ -16,12 +17,12 @@ class InvestigationFinder
     {
         $dbr = MediaWikiServices::getInstance()->getDBLoadBalancer()->getConnection(DB_REPLICA);
 
-        $searchtext = $dbr->addQuotes(strtolower("%$searchTerm%"));
+        $textFilterSQL = $this->getSearchTextConditions($searchTerm, $dbr);
         $publication = $dbr->addQuotes($publication->getDBkey());
         $sql = <<<SQL
 SELECT DISTINCT page_title, page_namespace FROM page 
     JOIN page_props props ON page_id = props.pp_page
-    WHERE LOWER(CONVERT(pp_value USING latin1)) LIKE $searchtext 
+    WHERE $textFilterSQL
     AND page_title LIKE CONCAT($publication,'/%');
 SQL;
         $res = $dbr->query($sql);
@@ -53,13 +54,14 @@ SQL;
             return [];
         }
 
-        $searchtext = $dbr->addQuotes(strtolower("%$searchTerm%"));
+        $textFilterSQL = $this->getSearchTextConditions($searchTerm, $dbr);
+
         $sql = <<<SQL
 SELECT DISTINCT t1.page_title, t1.page_namespace FROM page t1 JOIN 
     ( SELECT page_title FROM page p 
         JOIN page_props props ON p.page_id = props.pp_page
         JOIN category_index ON p.page_id = category_index.page_id AND category_index.category_id = $category_id
-        WHERE LOWER(CONVERT(pp_value USING latin1)) LIKE $searchtext
+        WHERE $textFilterSQL
        
     ) t2 ON t1.page_title LIKE CONCAT(t2.page_title,'/%');
 SQL;
@@ -103,5 +105,18 @@ SQL;
             ]
         );
         $out->addHTML($html);
+    }
+
+    private function getSearchTextConditions(string $searchTerm, IDatabase $dbr): string
+    {
+        $searchText = strtolower($searchTerm);
+        $parts = explode(' ', $searchText);
+        $parts = array_filter($parts, fn($e) => trim($e) !== '');
+        $conditions = [];
+        foreach ($parts as $part) {
+            $encoded = $dbr->addQuotes("%$part%");
+            $conditions[] = "LOWER(CONVERT(pp_value USING latin1)) LIKE $encoded";
+        }
+        return implode(" AND ", $conditions);
     }
 }
