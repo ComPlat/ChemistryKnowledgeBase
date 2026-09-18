f05192af9fbecfcc48b43910d784cf6b99fcd175
Kai Kühn <kuehn@diqa-pm.com>
2026-09-18 14:24:05 +0200

    moved WikiFarm config before env.php

--- mediawiki/LocalSettings.php
+++ mediawiki/LocalSettings.php
@@ -223,6 +223,27 @@ $wgGroupPermissions['sysop']['edit']            = true;
 $wgGroupPermissions['sysop']['createaccount']   = true;
 $wgGroupPermissions['sysop']['createpage']      = true;
 
+
+#################################################################
+# WikiFarm
+#################################################################
+# These settings are needed for the WikiGenerator (and can be overridden by env.php).
+# The default values here will only work on the Sarstedt servers.
+
+$mysqlBin         = 'mysql';
+$phpBin           = 'php';
+$publicHtml       = '/var/www/html';
+$solrBin          = getenv('WIKI_SOLR_BIN') ? getenv('WIKI_SOLR_BIN') : '/opt/solr-8.3.0/bin/solr';
+$wgWikiFarmAllowMissingEnv = true;
+$wgWikiFarmDefaultWikiId = 'main';
+$wgWikiFarmDBPattern = 'chem{wiki}';
+$wgWikiFarmDBPatternMappings = ['main' => 'chemmain143'];
+$wgWikiFarmScriptPathPattern = '/{wiki}/mediawiki';
+$wgWikiFarmEntryPoint = "/var/www/html/gateway";
+
+require_once ("extensions/WikiFarm/WikiSwitch.php");
+wfLoadExtension('WikiFarm');
+
 #################################################################
 #
 # Include local server settings, if they exist
@@ -461,26 +482,6 @@ wfLoadExtension( 'SyntaxHighlight_GeSHi' );
 #################################################################
 $wgExternalLinkTarget = '_blank';
 $wgCategoryTreeMaxDepth=4;
-
-#################################################################
-# WikiFarm
-#################################################################
-# These settings are needed for the WikiGenerator (and can be overridden by env.php).
-# The default values here will only work on the Sarstedt servers.
-
-$mysqlBin         = 'mysql';
-$phpBin           = 'php';
-$publicHtml       = '/var/www/html';
-$solrBin          = getenv('WIKI_SOLR_BIN') ? getenv('WIKI_SOLR_BIN') : '/opt/solr-8.3.0/bin/solr';
-$wgWikiFarmAllowMissingEnv = true;
-$wgWikiFarmDefaultWikiId = 'main';
-$wgWikiFarmDBPattern = 'chem{wiki}';
-$wgWikiFarmDBPatternMappings = ['main' => 'chemmain143'];
-$wgWikiFarmScriptPathPattern = '/{wiki}/mediawiki';
-$wgWikiFarmEntryPoint = "/var/www/html/gateway";
-
-require_once ("extensions/WikiFarm/WikiSwitch.php");
-wfLoadExtension('WikiFarm');
 
 #################################################################
 # Enhanced Retrieval
